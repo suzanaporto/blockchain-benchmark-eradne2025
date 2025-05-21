926bb690aff19bcf216ba464f690b3362005c3a4
washingtonpraxedes <washingtonpraxedes@gmail.com>
2025-05-21 19:17:37 -0300

    make automatic jwt update

--- smart-contract-client/client-experiment.js
+++ smart-contract-client/client-experiment.js
@@ -11,9 +11,20 @@ const privateKey = "3a94c5f2a379e6c47f86fecdfa82c9ff26997cc338d6b90373a2934901c9
 const infuraHost = "https://sepolia.infura.io/v3/0e8a3f90e08e40e2bfc900bab71aea16";
 // const infuraHost = "https://eth-mainnet.g.alchemy.com/v2/QnBVQjRWt3gaPx9_5T9Id_Z6Uomzl59G";
 
-const JWT_TOKEN = 'eyJraWQiOiJ6MERxcnFEbm9OR3BBYVI1Z2FMRkRHT3I5THhXb1Rkd2VFVHpsNGJYbzlJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiYWEyNmZkNS0yYzRjLTQzNzktYjUzYS0wZDI5NWFlOWU1N2YiLCJjdXN0b206c3ViZG9tYWluIjoiZGV2IiwiY3VzdG9tOmNvbXBhbnlHcm91cE5hbWUiOiJEZXYiLCJjdXN0b206RG9tYWluSXNUcmljb25uZWN0ZWQiOiIxIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImN1c3RvbTpnaXZlbl9uYW1lIjoiV2FzaGluZ3RvbiIsImN1c3RvbTpjb21wYW55R3JvdXBJZCI6Ijg4IiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfZnBtMjNhVFVRIiwiY29nbml0bzp1c2VybmFtZSI6Indhc2hpbmd0b25AdHJpbG9nby5jb20uYnIiLCJjdXN0b206ZG9tYWluVHlwZSI6IjEiLCJjdXN0b206dW5pcXVlX25hbWUiOiIxMjcyIiwiY3VzdG9tOnRpbWVab25lIjoiQW1lcmljYVwvU2FvX1BhdWxvIiwiYXVkIjoiNmpla2ZzNXI2aXZnYWdxYzdscGdkMzczZXIiLCJldmVudF9pZCI6ImI5N2VjYTlkLTQ2MGItNGI2ZS1hZGJjLTVkMTk1MWE0MWM2ZSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNzQ1ODcyMzE0LCJjdXN0b206ZmVhdHVyZV9mbGFncyI6IjYiLCJleHAiOjE3NDc3ODQ3NTAsImN1c3RvbTp1c2VyX3R5cGUiOiIxIiwiaWF0IjoxNzQ3NzgxMTUwLCJlbWFpbCI6Indhc2hpbmd0b25AdHJpbG9nby5jb20uYnIifQ.do0z3EyWyu1a2Asor3_-_7sAV6K_FoZvnhft4wbR6b7CiLqipRC4td7U6e5aKjd22RtOpf-lYeWjgGk1GLYUMt64lqQcTiTf6xoqYiP1bf9LtTdhQwyQUSERmY1E0e8PZwkVRHz2YVQXgZtBhor6dSTBprEHS5KhIO5ZgfrrjPSO3JKVwfI1yIyn7oH-VELI38dwXgFUktGv5ZYcDCMC5_npceWJ7KKqVwrH55FcFjGUs5NuIJkfFLufnXQFmluAnn_fAkqZrzm6psNQYGltYnjH_093PR97ytdtP5bB2AMU13QcTOAmBA8zDWlbqwWfOWl4NO67bd6Ogej6WcZYrw';
+
+const loginCredentials = {
+  UserEmail: 'beatriz@trilogo.com.br',
+  UserPassword: 'Ethereum123',
+};
+let JWT_TOKEN = 'eyJraWQiOiJ6MERxcnFEbm9OR3BBYVI1Z2FMRkRHT3I5THhXb1Rkd2VFVHpsNGJYbzlJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiYWEyNmZkNS0yYzRjLTQzNzktYjUzYS0wZDI5NWFlOWU1N2YiLCJjdXN0b206c3ViZG9tYWluIjoiZGV2IiwiY3VzdG9tOmNvbXBhbnlHcm91cE5hbWUiOiJEZXYiLCJjdXN0b206RG9tYWluSXNUcmljb25uZWN0ZWQiOiIxIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImN1c3RvbTpnaXZlbl9uYW1lIjoiV2FzaGluZ3RvbiIsImN1c3RvbTpjb21wYW55R3JvdXBJZCI6Ijg4IiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfZnBtMjNhVFVRIiwiY29nbml0bzp1c2VybmFtZSI6Indhc2hpbmd0b25AdHJpbG9nby5jb20uYnIiLCJjdXN0b206ZG9tYWluVHlwZSI6IjEiLCJjdXN0b206dW5pcXVlX25hbWUiOiIxMjcyIiwiY3VzdG9tOnRpbWVab25lIjoiQW1lcmljYVwvU2FvX1BhdWxvIiwiYXVkIjoiNmpla2ZzNXI2aXZnYWdxYzdscGdkMzczZXIiLCJldmVudF9pZCI6ImI5N2VjYTlkLTQ2MGItNGI2ZS1hZGJjLTVkMTk1MWE0MWM2ZSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNzQ1ODcyMzE0LCJjdXN0b206ZmVhdHVyZV9mbGFncyI6IjYiLCJleHAiOjE3NDc3ODQ3NTAsImN1c3RvbTp1c2VyX3R5cGUiOiIxIiwiaWF0IjoxNzQ3NzgxMTUwLCJlbWFpbCI6Indhc2hpbmd0b25AdHJpbG9nby5jb20uYnIifQ.do0z3EyWyu1a2Asor3_-_7sAV6K_FoZvnhft4wbR6b7CiLqipRC4td7U6e5aKjd22RtOpf-lYeWjgGk1GLYUMt64lqQcTiTf6xoqYiP1bf9LtTdhQwyQUSERmY1E0e8PZwkVRHz2YVQXgZtBhor6dSTBprEHS5KhIO5ZgfrrjPSO3JKVwfI1yIyn7oH-VELI38dwXgFUktGv5ZYcDCMC5_npceWJ7KKqVwrH55FcFjGUs5NuIJkfFLufnXQFmluAnn_fAkqZrzm6psNQYGltYnjH_093PR97ytdtP5bB2AMU13QcTOAmBA8zDWlbqwWfOWl4NO67bd6Ogej6WcZYrw';
 
 const datasetDir = '../chest_images/';
+
+const CREDENTIALS = {
+  nome: 'Washington Praxedes',
+  cpf: '12345678900'
+};
+
 
 async function uploadImage(filePath) {
     const form = new FormData();
@@ -38,10 +49,20 @@ async function uploadImage(filePath) {
     }
 }
 
-const CREDENTIALS = {
-  nome: 'Washington Praxedes',
-  cpf: '12345678900'
-};
+async function authenticateAndGetJWT() {
+  try {
+    const response = await axios.post(
+      'https://web.api.trilogo.app/api/Login/SignIn',
+      loginCredentials
+    );
+    const jwt = response.data?.accessToken || response.data?.accessToken;
+    if (!jwt) throw new Error('JWT not found in response');
+    return jwt;
+  } catch (error) {
+    console.error('Failed to authenticate and get JWT:', error.response?.data || error.message);
+    return null;
+  }
+}
 
 async function generateIdentificationHash() {
   try {
@@ -98,6 +119,8 @@ async function storeData(web3, storageContract, permalink, hashUser) {
 
 
 async function main() {
+    JWT_TOKEN = await authenticateAndGetJWT();
+
     let storage, web3 = await initBlockchain();
 
     const hash = await generateIdentificationHash();
